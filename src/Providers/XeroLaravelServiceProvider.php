14a6a1ddf781a5f78d65e23a21c2163fa102c15c
Jordan Hall <jordan@hall05.co.uk>
2018-10-16 11:44:38 +0100

    Fix accidentally removed semicolon

--- src/Providers/XeroLaravelServiceProvider.php
+++ src/Providers/XeroLaravelServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LangleyFoxall\XeroLaravel\Providers
+namespace LangleyFoxall\XeroLaravel\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use LangleyFoxall\XeroLaravel\Constants;
