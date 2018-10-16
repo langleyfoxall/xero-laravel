0ccee9806a60e646dcb95e78f36306718be1a5ff
Jordan Hall <jordan@hall05.co.uk>
2018-10-16 13:49:46 +0100

    Move methods to base XeroApp class

--- src/Apps/PrivateXeroApp.php
+++ src/Apps/PrivateXeroApp.php
@@ -2,63 +2,12 @@
 
 namespace LangleyFoxall\XeroLaravel\Apps;
 
-use BadMethodCallException;
-use LangleyFoxall\XeroLaravel\QueryWrapper;
-use XeroPHP\Models\Accounting\Contact;
-
 /**
  * Class XeroPrivateApp
  * @package LangleyFoxall\XeroLaravel
  */
 class PrivateXeroApp extends XeroApp
 {
-    /**
-     * Map between relationship names and Xero PHP library models
-     *
-     * @var array
-     */
-    private $relationshipToModelMap = [
-        'contacts' => Contact::class
-    ];
 
-    /**
-     * Call a relationship method, and return a QueryWrapper.
-     * Syntax: $xero->contacts()
-     *
-     * @param $name
-     * @param $arguments
-     * @return QueryWrapper
-     * @throws \XeroPHP\Remote\Exception
-     */
-    public function __call($name, $arguments)
-    {
-        $relationships = array_keys($this->relationshipToModelMap);
-
-        if (!in_array($name, $relationships)) {
-            throw new BadMethodCallException();
-        }
-
-        $model = $this->relationshipToModelMap[$name];
-
-        return new QueryWrapper($this->load($model));
-    }
-
-    /**
-     * Call a relationship method and get results.
-     * Syntax: $xero->contacts
-     *
-     * @param $name
-     * @return null
-     */
-    public function __get($name)
-    {
-        $relationships = array_keys($this->relationshipToModelMap);
-
-        if (!in_array($name, $relationships)) {
-            return null;
-        }
-
-        return $this->$name()->get();
-    }
 }
 
