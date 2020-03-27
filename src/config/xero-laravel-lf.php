9210f20d874110db8100a3e820a1d6117d5f1144
Jordan Hall <jordan@hall05.co.uk>
2020-03-27 09:41:15 +0000

    Remove token and tenant id from config

--- src/config/xero-laravel-lf.php
+++ src/config/xero-laravel-lf.php
@@ -14,13 +14,10 @@ return [
 
     'apps' => [
         'default' => [
-            'token'     => env('XERO_TOKEN'),
-            'tenant_id' => env('XERO_TENANT_ID'),
-
             'client_id'     => env('XERO_CLIENT_ID'),
             'client_secret' => env('XERO_CLIENT_SECRET'),
             'redirect_uri'  => env('XERO_REDIRECT_URI'),
-            'scope'         => env('XERO_SCOPE', 'openid email profile'),
+            'scope'         => env('XERO_SCOPE', 'openid email profile offline_access'),
         ],
     ],
 ];
