0d77c51b2eb8fb7983d65a54a0ed3003d066c68f
Jordan Hall <jordan@hall05.co.uk>
2020-03-30 00:20:15 +0100

    Update src/config/xero-laravel-lf.php
    Co-Authored-By: Dexter Marks-Barber <dexter@marks-barber.co.uk>

--- src/config/xero-laravel-lf.php
+++ src/config/xero-laravel-lf.php
@@ -17,7 +17,7 @@ return [
             'client_id'     => env('XERO_CLIENT_ID'),
             'client_secret' => env('XERO_CLIENT_SECRET'),
             'redirect_uri'  => env('XERO_REDIRECT_URI'),
-            'scope'         =>'openid email profile offline_access',
+            'scope'         => 'openid email profile offline_access',
         ],
     ],
 ];
